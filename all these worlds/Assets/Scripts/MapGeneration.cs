f4f74140c7275d5594cdf56d1611295bbb49da66
Florian CASTELAIN <castelainflorian44@gmail.com>
2020-09-28 08:49:18 +0200

    feat(mapgeneration): script to generate tile map

---
+++ all these worlds/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+
+    public int mapXLenght;
+    public int mapYLength;
+
+    public GameObject tile;
+
+    void GenerateMap(int maxX, int maxY) {
+        for (int x=0; x <= maxX; x++) {
+            for (int y=0; y <= maxY; y++) {
+                Instantiate(tile, new Vector3(10*x,0,10*y), Quaternion.identity);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap(mapXLenght, mapYLength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
